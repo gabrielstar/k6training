afe1c7149c6a605449cff3d7a5f060c3b614692b
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-08-10 14:02:15 +0200

    k6 training

---
+++ 001_javascript_in_15_mins/02_cwiczenie.md
@@ -0,0 +1,14 @@
+## Javascript
+
+
+1. Zadeklaruj listę kilku obiektów o następującej strukturze:
+    {
+        name: ...
+        age: ..
+        voice: 
+    }
+2. Voice powinno być funkcją
+3. Ziteruj po całej liście i dla każdego obiektu
+-  wyświetl name i age,
+- wykonaj funkcje voice
+4. Zadanie zrealizuj w snippetach w Google Chrome
---
+++ 001_javascript_in_15_mins/03_odpowiedz.js
@@ -0,0 +1,38 @@
+//via constructor similar to c# and java
+class OOO {
+    constructor(name, age, voice) {
+        this.name = name;
+        this.age = age;
+        this.voice = voice;
+    }
+}
+var ooo = new OOO("Witek", 22, () => {
+    console.log("aasadasdsd")
+});
+//via constructor function
+var OO = function (name, age, voice) {
+    this.name = name;
+    this.age = age;
+    this.voice = voice;
+}
+var oo = new OO("Romek", 12, function () {
+    console.log("blrlrllr")
+})
+//via object literal
+var o = {
+    name: "Gabriel",
+    age: 37,
+    voice: function () {
+        console.log("arghhh");
+    }
+};
+
+//add to array at the beginning or end
+var arr = [ooo];
+arr.push(o);
+arr.unshift(oo);
+
+for (var obj of arr) { //of not in
+    console.log(obj['name'] + ' at ' + obj.age + " gives a sound like ")
+    obj.voice()
+}
