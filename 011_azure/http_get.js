441c95b807e428afe4a679c261ead3d9890d966a
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-08-13 12:17:40 +0200

    a

--- 011_azure/http_get.js
+++ 011_azure/http_get.js
@@ -1,5 +1,17 @@
 import http from 'k6/http';
 
+const smokeOptions = { //options allow to configure your test rum
+    vus: 1,
+    iterations: 1,
+};
+
+const stressOptions = { //options allow to configure your test rum
+    vus: 1,
+    iterations: 5,
+};
+
+export let options = eval(__ENV.options) || smokeOptions
+
 export default function () { //https://stackoverflow.com/questions/21117160/what-is-export-default-in-javascript
     http.get('https://appxx.azurewebsites.net/');
 }
