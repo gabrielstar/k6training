a97b80eaddb23a9cc264b024eae0eb41540339a2
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-08-14 11:03:22 +0200

    first draft

--- 003_basic_test/http_get.js
+++ 003_basic_test/http_get.js
@@ -2,6 +2,8 @@ import http from 'k6/http';
 
 //simplest test possible
 export default function () { //https://stackoverflow.com/questions/21117160/what-is-export-default-in-javascript
+    //początek iteracji
     let res = http.get('https://appxx.azurewebsites.net/'); //request - response
     console.log(JSON.stringify(res)); //The JSON.stringify() method converts a JavaScript object or value to a JSON string
+    //koniec iteracji
 }
