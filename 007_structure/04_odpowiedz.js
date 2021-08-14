a9ebd2e9e3de358682093dbf4496068135523806
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-08-14 15:03:28 +0200

    first draft

--- 007_structure/04_odpowiedz.js
+++ 007_structure/04_odpowiedz.js
@@ -2,11 +2,12 @@ import http from 'k6/http';
 import {check} from 'k6';
 
 const URL = 'https://appxx.azurewebsites.net/'
+var counter = 0; //każdy VU dostanie kopię
 
 export function setup() {
     let res = http.get(URL);
     return { //data
-        body: res.body
+        body: res.body //body is a property
     }
 }
 export default function (data) {
@@ -26,9 +27,14 @@ export default function (data) {
     //modify
     data['new'] = "new";
     console.log(JSON.stringify(data));
+
+    //dane z init są kopiowane, kopia per VU
+    counter+=Math.random(1)*100;
+    console.log(`counter:` + counter) //pracujemy na kopii w zakresie 1 VU
     return data;
 }
 
 export function teardown(data) {
     console.log(JSON.stringify(data)); //data is unmodified
+    console.log(`counter:` + counter) //nadal wskazuje na 0
 }
