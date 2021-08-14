9439d4350508777f1d62b4f021a7267da43f0170
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-08-14 11:00:56 +0200

    first draft

--- 003_basic_test/http_get.js
+++ 003_basic_test/http_get.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 
+//simplest test possible
 export default function () { //https://stackoverflow.com/questions/21117160/what-is-export-default-in-javascript
     let res = http.get('https://appxx.azurewebsites.net/'); //request - response
     console.log(JSON.stringify(res));
--- 003_basic_test/http_post.js
+++ 003_basic_test/http_post.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
 
+//test with all params set
 export default function () {
     var url = 'https://appxx.azurewebsites.net/post/add/newpost';
     var payload = JSON.stringify(
