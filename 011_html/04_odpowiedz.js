97e034846341683e74d1bf446105cb93ff6b6cd0
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-08-15 11:06:36 +0200

    first draft

--- 011_html/04_odpowiedz.js
+++ 011_html/04_odpowiedz.js
@@ -1,23 +1,23 @@
 import http from 'k6/http';
 import {parseHTML} from 'k6/html';
-import {check, group, batch} from 'k6';
+import {check, group} from 'k6';
 
 const URL = 'https://red-water-022d04b03.azurestaticapps.net/';
 export let options = {
     batch: 2 //max parallel request in batch
 }
-export default function () { //https://stackoverflow.com/questions/21117160/what-is-export-default-in-javascript
+export default function () {
     group('Get Main Page', function () {
         let res = http.get(URL);
         let embeddedSources = getEmbeddedSource(res);
         group('Get Embedded Resources', function () { //iterate over embedded resources and download
             let parallelRequests = []
             embeddedSources.forEach(
-                function (resource) {
+                (resource) => {
                     if (resource.startsWith('http')) {
-                        parallelRequests.push(['GET',resource])
+                        parallelRequests.push(['GET', resource])
                     } else {
-                        parallelRequests.push(['GET',URL+resource])
+                        parallelRequests.push(['GET', URL + resource])
                     }
                 }
             )
@@ -29,6 +29,7 @@ export default function () { //https://stackoverflow.com/questions/21117160/what
     });
 
 }
+
 /*
     Get all > src and link > href sources
  */
@@ -36,13 +37,14 @@ function getEmbeddedSource(res) {
     let sources = [];
     let source;
     const req_script = parseHTML(res.body);
-    req_script.find('script,img,link').toArray().forEach(function (item) {
+    console.log("Sources detected: " )
+    req_script.find('script,img,link').toArray().forEach((item) => {
         if (item.attr('src')) {
             source = item.attr('src');
         } else if (item.attr('href')) {
             source = item.attr('href');
         }
-        console.log("Source detected: " + source)
+        console.log(" -- " + source)
         sources.unshift(source)
     });
     return sources;
