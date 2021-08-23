a3b40dec9aeb10b52dd261b2390415bfa88bb4dc
Gabriel Starczewski <38763186+gabrielstar@users.noreply.github.com>
2021-08-23 14:19:41 +0200

    Update impl.js

--- 005_checks/impl.js
+++ 005_checks/impl.js
@@ -1,1 +1,13 @@
-
+//k6 use some strange structure - see why 
+//dummy check implementation
+let check = (x, checks) => {
+  for (let c in checks) {
+    console.log(`${c} => ${checks[c].call(this, x)}`);
+  }
+}
+//response
+let res = "res";
+check(res, {
+  'should be true': (res) => res === "res",
+  'a a': () => true
+});
