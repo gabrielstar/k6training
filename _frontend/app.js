2c1a593b4ef4c01d990742a1cd80eecc99c1506a
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-09-20 09:57:57 +0200

    first draft

--- _frontend/app.js
+++ _frontend/app.js
@@ -1,22 +1,29 @@
 const $ = document;
 registerEvents();
 
-function registerEvents(){
+function registerEvents() {
     $.addEventListener('DOMContentLoaded', displayPosts);
+    $.addEventListener('DOMContentLoaded', resetContent);
 }
 
 function displayPosts(e) {
     fetch("http://localhost:3000/posts")
-        .then(res=>res.json())//return posts
-        .then(posts=>{
+        .then(res => res.json())//return posts
+        .then(posts => {
             console.log(posts);
             let html = '';
-            posts.forEach(post=>{
-              html+=`<li>${post.title}</li>`
+            posts.forEach(post => {
+                html += `<li>${post.title}</li>`
             })
             $.getElementById('container').innerHTML = `<ul>${html}</ul>`;
         })
         .catch(error => {
             console.log(error);
         })
+}
+
+function resetContent(e) {
+    setTimeout(() => {
+        $.getElementById('container').innerHTML = '<p>Reset</p>'
+    }, 3000)
 }
