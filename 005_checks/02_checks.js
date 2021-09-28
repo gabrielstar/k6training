116fc8e5adae630bbf32607ac987cc7ac8ba7ed7
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-09-28 09:23:23 +0200

    fixes

--- 005_checks/02_checks.js
+++ 005_checks/02_checks.js
@@ -5,9 +5,10 @@ const URL = "https://appxx.azurewebsites.net/"
 
 export default function () {
     let res = http.request('GET', URL);
+    console.log(JSON.stringify(res, null, 2))
     let checkResult = check(res, {
         'response code was 200': (res) => res.status == 200,
-        'body size was 1234 bytes': (res) => res.body.length < 1234,
+        'remote port was 443': (res) => res.remote_port === 443,
         'dummy check' : ()=> 'a' === 'a'
     });
     console.log("Check Result " + checkResult)
--- 005_checks/04_odpowiedz.js
+++ 005_checks/04_odpowiedz.js
@@ -1,12 +1,12 @@
 import http from 'k6/http';
-import {check} from 'k6';
+import { check } from 'k6';
 
 const URL = "https://appxx.azurewebsites.net/"
 
 export default function () {
     let res = http.request('GET', URL);
     let checkResult = check(res, {
-        'response code was 200': (res) => res.status == 200,
+        'response code was 200': function (res) { return true; },
         'tcp connect < 100': (res) => res.timings.connecting < 100,
         'k6 is User-Agent': (res) => res.request.headers['User-Agent'][0].includes("k6"),
     });
